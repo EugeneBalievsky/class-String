a4332e840fd0b9919b83b34eaa7c722e6d72439d
EugeneBalievsky <111220873+EugeneBalievsky@users.noreply.github.com>
2022-09-28 17:39:00 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ private:
     size_t size;
     size_t capacity;
 
-    void resize(int new_capacity);  //ќн будет увеличивать capacity в 2 раза
+    void resize(int new_capacity);
 public:
     String() : size(1), capacity(1), str(new char[capacity]) {
     }
@@ -31,22 +31,35 @@ public:
     String& operator+=(char c);
 
     void print();
-    // дописать все методы
+    //ћетод должен вернуть новую строку, в которой все буквы верхнего регистра стали буквами нижнего регистра
     String tolower()const;
+
+    //ћетод должен вернуть новую строку, в которой все буквы верхнего регистра стали буквами верхнего регистра
     String toupper()const;
-    void reverse(int start, int end);
+
+    //–азворот строки
+    void reverse(int start, int end);   //pos - с какой позиции, end - по какую (включительно)
+
+    //»щет первое вхождение строки other в строку, к которой примен€етс€ метод.
+    //≈сли вхождени€ нет - то возвращаем -1
+    /*
+     ѕример:
+     String s = "abcbabccc";
+     String other = "bab";
+     std::cout << s.find(); //¬ыведет 3
+     */
     int find(const String& other);
+
 };
 
-String::String(const char* s): 
-    size(strlen(s)), capacity(size*2), str(new char[strlen(s)*2]){ 
+String::String(const char* s) :
+    size(strlen(s)), capacity(size * 2), str(new char[strlen(s) * 2]) {
     for (int i = 0; i < size; ++i) {
         str[i] = s[i];
     }
 }
-String::String(const String& s) {
-    size = s.size;
-    capacity = s.capacity;
+String::String(const String& s) :
+    size(s.size), capacity(s.capacity), str(new char[s.capacity]) {
     for (int i = 0; i < size; ++i) {
         str[i] = s.str[i];
     }
@@ -71,17 +84,108 @@ void String::print() {
     std::cout << '\n';
 }
 String& String::operator=(const String& other) {
-    size = other.size;
-    capacity = other.capacity;
-    for (int i = 0; i < size; ++i) {
-        str[i] = other.str[i];
+    if (str = other.str) {
+        return *this;
     }
+    String new_string;
+    new_string.size = other.size;
+    new_string.capacity = other.capacity;
+    for (int i = 0; i < size; ++i) {
+        new_string.str[i] = other.str[i];
+    }
+    return new_string;
+}
+bool operator == (const String& left, const String& right) {
+    if (left.size == right.size) {
+        for (int i = 0; i < left.size; ++i) {
+            if (left.str[i] != right.str[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+bool operator != (const String& left, const String& right) {
+    return !(left == right);
+}
+bool operator < (const String& left, const String& right) {
+    if (left == right) {
+        return false;
+    }
+    for (int i = 0; i < std::max(left.size, right.size); ++i) {
+        if (left.str[i] > right.str[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+bool operator <=(const String& left, const String& right) {
+    return left == right || left < right;
+}
+bool operator > (const String& left, const String& right) {
+    return !(left <= right);
+}
+bool operator >= (const String& left, const String& right) {
+    return !(left < right);
+}
+String operator+(const String& left, const String& right) {
+    String s;
+    s.size = left.size + right.size;
+    s.capacity = left.capacity + right.capacity;
+    s.str = new char[s.capacity];
+    for (int i = 0; i < left.size ; ++i) {
+        s.str[i] = left.str[i];
+    }
+    for (int i = left.size; i < s.size; ++i) {
+        s.str[i] = right.str[i- left.size];
+    }
+    return s;
+}
+String operator+(const String& left, char c) {
+    String s;
+    s.size = left.size + 1; 
+    s.capacity = left.capacity + 1;
+    s.str = new char[s.capacity];
+    for (int i = 0; i < s.size - 1; ++i) {
+        s.str[i] = left.str[i];
+    }
+    s.str[s.size - 1] = c;
+    return s;
+}
+String& String::operator += (const String& right) {
+    
+    return *this;
+}
+String& String:: operator+=(char c) {
+    *this = *this + c;
     return *this;
 }
 
 int main() {
-    String s = "jdfhgj";
+    String s = "kjhh";
+    String ss("acdsahkh");
     s.print();
+    ss.print();
+
+    if (s > ss) {
+        std::cout << "yes"<<'\n';
+    }
+    else
+        std::cout << "no"<<'\n';
+
+    String dd = s + ss;
+    dd.print();
+
+    String d = ss + 'a';
+    d.print();
+
+    d += 's';
+    d.print();
+
+
 
     return 0;
 }
